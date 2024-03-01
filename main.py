fdcc09964735c843e3e2f68c41271b278de60cb2
Marcela DL <leon.ari00@hotmail.com>
2024-03-01 19:11:04 +0100

    Access data

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import requests
 
 api_key = "d0b3301d6d00484698f336b19b927e27"
-url = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=d0b3301d6d00484698f336b19b927e27"
+url = "https://newsapi.org/v2/everything?q=apple&from=2024-02-29&to=2024-02-29&sortBy=popularity&apiKey=d0b3301d6d00484698f336b19b927e27"
 
+#Make a request 
 request = requests.get(url)
+
+#Got a dictionary with data
 content = request.json()
-print(content["articles"])
+
+#Access the data 
+for article in content["articles"]:
+    print(article["title"])
+    print(article["description"])
