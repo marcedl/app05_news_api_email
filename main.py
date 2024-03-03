1d71c87912e60ca5cf61035b13ad5b622dec255a
Marcela DL <leon.ari00@hotmail.com>
2024-03-03 17:37:03 +0100

    Add specific language, limit of news, subject, control category

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import requests
 from send_email import send_email
 
+topic = "tesla"
 api_key = "d0b3301d6d00484698f336b19b927e27"
-url = "https://newsapi.org/v2/everything?q=apple&from=2024-02-29&to=2024-02-29&sortBy=popularity&apiKey=d0b3301d6d00484698f336b19b927e27"
+url = f"https://newsapi.org/v2/everything?q={topic}&from=2024-02-03&sortBy=publishedAt&apiKey=d0b3301d6d00484698f336b19b927e27&language=en"
 
 #Make a request 
 request = requests.get(url)
@@ -11,9 +12,9 @@ request = requests.get(url)
 content = request.json()
 
 #Access the data 
-body = ""
-for article in content["articles"]:
-    if article["title"] is not None:
+body = "Subject: Today's Tesla News" + "\n" 
+for article in content["articles"][:20]:
+    if (article["description"] is not None) & (article["title"] is not None):
         body = body + article["title"] + "\n" + article["description"] + "\n" + article["url"] + 2*"\n"
        
 body = body.encode("utf-8")
